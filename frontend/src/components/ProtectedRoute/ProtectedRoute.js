dbd80ff9df3d784230867231cccbdc2c7aa4421c
113870-Lokman-Matias <113870@tecnicatura.frc.utn.edu.ar>
2024-04-05 21:32:26 -0300

    Revert " feat: Frontend de usuarios implementado"
    This reverts commit 545af27146e09794401ec99f79d8ec3919985012.

--- frontend/src/components/ProtectedRoute/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export const ProtectedRoute = ({ children, redirectTo = "/login" }) => {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    return <Navigate to={redirectTo} />;
-  }
-  return children ? children : <Outlet />;
-};
